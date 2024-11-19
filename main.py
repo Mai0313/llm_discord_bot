bad2fdb1e20a9db8668d5df5de7fbc93ea3f8011
wei <weichenglee0313@gmail.com>
2024-11-20 00:06:42 +0800

    refactor: 重命名 LLMReplyCogs 中的指令名稱

--- main.py
+++ main.py
@@ -3,35 +3,35 @@ from pathlib import Path
 import discord
 import logfire
 from discord.ext import commands
-from src.sdk.llm import LLMServices
-from src.types.config import Config
 
 logfire.configure()
-
-config = Config()
-llm_services = LLMServices()
 
 # 啟用所有 Intents
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
+discord_invite_url = (
+    "https://discord.com/oauth2/authorize?client_id={client_id}&permissions=8&scope=bot"
+)
 
 
 @bot.event
 async def on_ready() -> None:
     app_info = await bot.application_info()
-    invite_url = (
-        f"https://discord.com/oauth2/authorize?client_id={app_info.id}&permissions=8&scope=bot"
-    )
-    logfire.info("Bot Started", bot_name=bot.user.name, bot_id=bot.user.id)
-    logfire.info(f"Invite Link: {invite_url}")
-    logfire.info("Loading Cogs...")
+    invite_url = discord_invite_url.format(client_id=app_info)
+
     cog_path = Path("./src/cogs")
     cog_files = [f.stem for f in cog_path.glob("*.py") if not f.stem.startswith("__")]
     for cog_file in cog_files:
         filename = f"src.cogs.{cog_file}"
-        logfire.info("Loading Cogs", filename=filename)
         await bot.load_extension(filename)
+        logfire.info("Cogs Loaded", filename=filename)
+
+    logfire.info("Bot Started", bot_name=bot.user.name, bot_id=bot.user.id)
+    logfire.info(f"Invite Link: {invite_url}")
 
 
 if __name__ == "__main__":
+    from src.types.config import Config
+
+    config = Config()
     bot.run(token=config.discord_bot_token, log_handler=logfire.LogfireLoggingHandler())
