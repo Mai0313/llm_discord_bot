40b9f2611aa7e107aa4e9623741f0c02e3a61f00
wei <weichenglee0313@gmail.com>
2024-11-26 00:13:26 +0800

    refactor: 更新環境設定檔案
    更新了`.env.example`檔案中的Generative AI和Discord Bot的相關設定，以及新增了PostgreSQL和Redis的配置。

--- src/cogs/gen_reply.py
+++ src/cogs/gen_reply.py
@@ -1,4 +1,3 @@
-from discord import app_commands
 from discord.ext import commands
 
 from src.sdk.llm import LLMServices
---
+++ src/types/database.py
@@ -0,0 +1,98 @@
+from typing import Optional
+
+from pydantic import Field, AliasChoices, computed_field
+from sqlalchemy.orm import sessionmaker
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
+
+
+class PostgreSQLConfig(BaseSettings):
+    postgres_host: str = Field(
+        ...,
+        validation_alias=AliasChoices("POSTGRES_HOST"),
+        title="PostgreSQL Host",
+        description="The hostname or IP address of the PostgreSQL server.",
+    )
+    postgres_port: str = Field(
+        ...,
+        validation_alias=AliasChoices("POSTGRES_PORT"),
+        title="PostgreSQL Port",
+        description="The port number on which the PostgreSQL server is listening.",
+    )
+    postgres_db: str = Field(
+        ...,
+        validation_alias=AliasChoices("POSTGRES_DB"),
+        title="PostgreSQL Database Name",
+        description="The name of the PostgreSQL database to connect to.",
+    )
+    postgres_user: str = Field(
+        ...,
+        validation_alias=AliasChoices("POSTGRES_USER"),
+        title="PostgreSQL User",
+        description="The username used to authenticate with the PostgreSQL server.",
+    )
+    postgres_password: str = Field(
+        ...,
+        validation_alias=AliasChoices("POSTGRES_PASSWORD"),
+        title="PostgreSQL Password",
+        description="The password used to authenticate with the PostgreSQL server.",
+    )
+
+    @computed_field
+    @property
+    def postgres_dsn(self) -> str:
+        return f"postgresql://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+
+    @computed_field
+    @property
+    def postgres_async_engine(self) -> AsyncEngine:
+        return create_async_engine(self.postgres_dsn)
+
+    @computed_field
+    @property
+    def postgres_async_session(self) -> sessionmaker:
+        return sessionmaker(
+            self.postgres_async_engine, expire_on_commit=False, class_=AsyncSession
+        )
+
+
+class SQLiteConfig(BaseSettings):
+    sqlite_file_path: str = Field(
+        ...,
+        validation_alias=AliasChoices("SQLITE_FILE_PATH"),
+        title="SQLite File Path",
+        description="The file path to the SQLite database file.",
+    )
+    sqlite_timeout: int = Field(
+        default=30,
+        validation_alias=AliasChoices("SQLITE_TIMEOUT"),
+        title="SQLite Timeout",
+        description="The timeout duration (in seconds) for SQLite operations. Defaults to 30 seconds.",
+    )
+
+
+class RedisConfig(BaseSettings):
+    redis_host: str = Field(
+        ...,
+        validation_alias=AliasChoices("REDIS_HOST"),
+        title="Redis Host",
+        description="The hostname or IP address of the Redis server.",
+    )
+    redis_port: int = Field(
+        default=6379,
+        validation_alias=AliasChoices("REDIS_PORT"),
+        title="Redis Port",
+        description="The port number on which the Redis server is listening. Defaults to 6379.",
+    )
+    redis_db: int = Field(
+        default=0,
+        validation_alias=AliasChoices("REDIS_DB"),
+        title="Redis Database Index",
+        description="The database index to connect to on the Redis server. Defaults to 0.",
+    )
+    redis_password: Optional[str] = Field(
+        default=None,
+        validation_alias=AliasChoices("REDIS_PASSWORD"),
+        title="Redis Password",
+        description="The password used to authenticate with the Redis server, if required.",
+    )
