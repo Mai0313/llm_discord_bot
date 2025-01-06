01535683ecc2a898b99d41bc182525793028ffd9
wei <weichenglee0313@gmail.com>
2025-01-07 00:15:35 +0800

    refactor: 調整 huggingface_api_token 的位置，確保配置項目的一致性

--- src/types/config.py
+++ src/types/config.py
@@ -6,19 +6,19 @@ logfire.configure()
 
 
 class Config(BaseSettings):
-    huggingface_api_token: str = Field(
-        ...,
-        description="The api token from huggingface for calling models.",
-        examples=["hf_zdZ..."],
-        alias="HUGGINGFACE_API_TOKEN",
-        frozen=False,
-        deprecated=False,
-    )
     openai_api_key: str = Field(
         ...,
         description="The api key from openai for calling models.",
         examples=["sk-proj-..."],
         alias="OPENAI_API_KEY",
+        frozen=False,
+        deprecated=False,
+    )
+    huggingface_api_token: str = Field(
+        ...,
+        description="The api token from huggingface for calling models.",
+        examples=["hf_zdZ..."],
+        alias="HUGGINGFACE_API_TOKEN",
         frozen=False,
         deprecated=False,
     )
